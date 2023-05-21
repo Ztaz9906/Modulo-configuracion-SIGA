5b70173818741922c4ce66156fc6e0a5ce4c190f
Ztaz9906 <ztaz9906@gmail.com>
2023-05-21 04:54:48 -0400

    ya no redirecciona al registrar

--- adminschema/urls.py
+++ adminschema/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import TbInstitucionViewSet
-from dj_rest_auth.registration.views import RegisterView
+from .views import TbInstitucionViewSet,CustomRegisterView
 from dj_rest_auth.views import LoginView, LogoutView
 router = routers.DefaultRouter()
 ################ Nuevo modelo #################################
@@ -13,7 +12,7 @@ router.register(r'Instituciones', TbInstitucionViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     # Endpoint para registrar usuarios
-    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/register/', CustomRegisterView.as_view(), name='register'),
     # Endpoints para logear y deslogear usuarios
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
--- adminschema/views.py
+++ adminschema/views.py
@@ -1,15 +1,24 @@
 from rest_framework import mixins,viewsets
-from django.contrib.auth.models import User
-from .serializer import TbUserCreateSerializer
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.response import Response
 from .models import TbUser, TbInstitucion
 from .serializer import TbInstitucionSerializer
 from rest_framework import status
-from django.contrib.auth.hashers import check_password, make_password
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.registration.serializers import RegisterSerializer
 ################ Nuevo modelo #################################
 class TbInstitucionViewSet(viewsets.ModelViewSet):
     queryset = TbInstitucion.objects.all()
     serializer_class = TbInstitucionSerializer
 ################   final     ################################
+class CustomRegisterView(RegisterView):
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save(self.request)
+        return Response(self.get_response_data(user))
+
+    def get_response_data(self, user):
+        return {'detail': 'Usuario registrado con éxito.'}
+
